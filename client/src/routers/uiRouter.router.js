93593042fd89c14f866c70e6c2015ce975e71715
choudharysidharth082000 <choudharysidharth082000@gmail.com>
2023-02-17 23:27:22 +0530

    Added Nested Routes in UI router

--- client/src/routers/uiRouter.router.js
+++ client/src/routers/uiRouter.router.js
@@ -1,24 +1,30 @@
 ///<reference path="../../index.js" />
 //creating the routes for the app
 myapp.config(function ($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/signup');
-      $stateProvider
-        .state('login', {
-          url: '/login',
-          templateUrl: 'src/pages/login.html'
-        })
-        .state('signup', {
-          url: '/signup',
-          templateUrl: 'src/pages/signup.html',
-        })
-        .state('dashboard', {
-            url: '/dashboard',
-            templateUrl: 'src/pages/dashboard.html',
-          })
-        .state('analytics', 
-        {
-          url: '/analytics',
-          templateUrl: 'src/pages/analytics.html',
-        })
+  $urlRouterProvider.otherwise("/signup");
+  $stateProvider
+    .state("login", {
+      url: "/login",
+      templateUrl: "src/pages/login.html",
+    })
+    .state("signup", {
+      url: "/signup",
+      templateUrl: "src/pages/signup.html",
+    })
+    .state("dashboard", {
+      url: "/dashboard",
+      templateUrl: "src/pages/dashboard.html",
+    })
+    .state("dashboard.users", {
+      url: "/users",
+      templateUrl: "src/pages/dashboard/user.html",
+    })
+    .state("dashboard.campaign", {
+      url: "/campaign",
+      templateUrl: "src/pages/dashboard/campaign.html",
+    })
+    .state("analytics", {
+      url: "/analytics",
+      templateUrl: "src/pages/analytics.html",
     });
-    
+});
